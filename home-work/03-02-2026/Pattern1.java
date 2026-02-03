649cf7548438ff103aeadcb8b54b5adf95cda533
raut-abhishek <iamabhishekraut01@gmail.com>
2026-02-03 12:37:28 +0530

    printing pattern

---
+++ home-work/03-02-2026/Pattern1.java
@@ -0,0 +1,19 @@
+
+
+public class Pattern1 {
+    public static void main(String[] args){
+        int n = 5;
+        for(int i = 1; i<=(n*2)-1; i++){
+            if(i<=n){
+                for(int j =i; j>=1; j--){
+                    System.out.print("*");
+                }
+            }else{
+                for(int j =i; j<=(n*2)-1; j++){
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
