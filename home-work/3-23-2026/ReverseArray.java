026f90131a5a5f47b110f0c917fd1b6eb57e4670
raut-abhishek <iamabhishekraut01@gmail.com>
2026-03-22 23:51:42 +0530

    reverse an array

---
+++ home-work/3-23-2026/ReverseArray.java
@@ -0,0 +1,23 @@
+// package home-work.3-23-2026;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            arr[start] = arr[start] ^ arr[end];
+            arr[end] = arr[start] ^ arr[end];
+            arr[start] = arr[start] ^ arr[end];
+
+            start++;
+            end--;
+        }
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
