2d00c11004c799453dd745b6003844cd3e424f52
raut-abhishek <iamabhishekraut01@gmail.com>
2026-03-24 13:35:18 +0530

    added counter for tracking

--- home-work/3-24-2026/AddCountToFirst.java
+++ home-work/3-24-2026/AddCountToFirst.java
@@ -12,14 +12,16 @@ public class AddCountToFirst {
                 temp /= 10;
             }
 
-            int sum = i * multiFactor + a[i];
+            int count = i + 1;
+            int sum = count * multiFactor + a[i];
+
             a[i] = sum;
         }
         return a;
     }
 
     public static void main(String[] args){
-        int[] arr = {10,20,30,40,50,60,70,80,80,90,100};
+        int[] arr = {10,20,30,40,50,60,70,80,80,90,100, 110,120};
         System.out.println(Arrays.toString(addCount(arr)));
     }
 }
