74e8725d203dd1b37346c8712e518ecc31e12def
raut-abhishek <iamabhishekraut01@gmail.com>
2026-03-25 23:38:15 +0530

    Two sum problem

---
+++ home-work/3-25-2026/FindSumInArray.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class FindSumInArray {
+
+    static int[] findSum(int[] a, int target){
+        for(int i = 0; i<a.length; i++){
+            for(int j = i; j<a.length; j++){
+                if(a[i] + a[j] == target){
+                    return new int[]{a[i], a[j]};
+                }
+            }
+        }
+        return new int[-1];
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {45,3,7,53,27,64,25,45,97,33,90};
+        int sum = 187;
+        System.out.println(Arrays.toString(findSum(arr, sum)));
+    }   
+}
