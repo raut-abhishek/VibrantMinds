668acd103b90153b32560b65b52538575bc6ae92
raut-abhishek <iamabhishekraut01@gmail.com>
2026-04-02 23:38:58 +0530

    added coments

--- java/3-31-2026/SelectionSort.java
+++ java/3-31-2026/SelectionSort.java
@@ -2,11 +2,11 @@ import java.util.Arrays;
 
 public class SelectionSort {
 
-    static int[] sortArray(int[] a){
-        for(int i = 0; i<a.length; i++){
+    static int[] sortArray(int[] a) {
+        for (int i = 0; i < a.length; i++) {
             int current = i;
-            for(int j = i+1; j<a.length; j++){
-                if(a[j]<a[current]){
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j] < a[current]) {
                     current = j;
                 }
             }
@@ -17,10 +17,13 @@ public class SelectionSort {
         return a;
     }
 
-
-
     public static void main(String[] args) {
-        int[] arr = {5,4,9,3,7,1,8,2,6,10};
+        int[] arr = { 5, 4, 9, 3, 7, 1, 8, 2, 6, 10 };
+        //          { 5, 4, 9, 3, 7, 1, 8, 2, 6, 10 }
+        //            i
+        //               j
+        //               c
+
         System.out.print(Arrays.toString(sortArray(arr)));
     }
 }
