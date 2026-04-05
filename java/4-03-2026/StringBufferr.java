3a8a969f82b63cc8580d36f17ab018c18f44773e
raut-abhishek <iamabhishekraut01@gmail.com>
2026-04-05 22:43:19 +0530

    creating string using StringBuffer class

---
+++ java/4-03-2026/StringBufferr.java
@@ -0,0 +1,9 @@
+import java.lang.StringBuffer;
+
+public class StringBufferr {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("DIGITAL");
+        sb.append(" INDIA");
+        System.out.println(sb);
+    }
+}
