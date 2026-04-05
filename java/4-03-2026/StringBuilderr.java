c5289bf8d651a9949007ab965ffcb17ad78922a9
raut-abhishek <iamabhishekraut01@gmail.com>
2026-04-05 22:40:02 +0530

    StringBUilder class example

---
+++ java/4-03-2026/StringBuilderr.java
@@ -0,0 +1,10 @@
+import java.lang.StringBuilder;
+
+public class StringBuilderr {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("INDIA");
+        sb.append(" IS");
+        sb.append(" GREAT");
+        System.out.println(sb);
+    }
+}
