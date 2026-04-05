67bf793fc99dfed310c841d9d7e0ea181a2f34e1
raut-abhishek <iamabhishekraut01@gmail.com>
2026-04-05 16:22:12 +0530

    Taking input from user using BufferedReade and InputStreamReader

---
+++ java/4-04-2026/InputMethod1.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class InputMethod1 {
+    public static void main(String[] args)throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        
+        System.out.print("Enter A: ");
+        int a = Integer.parseInt(br.readLine());
+        
+        System.out.print("Enter B: ");
+        int b = Integer.parseInt(br.readLine());
+
+        System.out.print("A + B: " + (a+b));
+    }
+}
