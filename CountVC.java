6d8b5a06d640e72791ce76da48847c14828eca9d
raut-abhishek <iamabhishekraut01@gmail.com>
2026-04-06 23:58:16 +0530

    Count Vowels and Consonants

---
+++ CountVC.java
@@ -0,0 +1,23 @@
+public class CountVC {
+    public static void main(String[] args) {
+        String str = "hello world";
+        int vowels = 0, consonants = 0;
+
+        str = str.toLowerCase();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                if ("aeiou".indexOf(ch) != -1) {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+}
