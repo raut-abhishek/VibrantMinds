3dcd88599e53356e72db7c6ae45c2ef84760876f
raut-abhishek <iamabhishekraut01@gmail.com>
2026-04-25 00:02:32 +0530

    predefined consumer functional interface

--- java/4-23-2026/ConsumerInterface.java
+++ java/4-23-2026/ConsumerInterface.java
@@ -1,7 +1,12 @@
 import java.util.function.*;
 
-public class ConsumerInterface{
-    public static void  main(String[] args){
-        
+public class ConsumerInterface {
+    public static void main(String[] args) {
+        Consumer<Integer> c1 = (Integer a) -> {
+            for (int i = 0; i <= a; i++) {
+                System.out.print(i + " ");
+            }
+        };
+        c1.accept(10);
     }
 }
