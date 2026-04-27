35ed7b70bd9bd37b71b4922ecbbad3a5331add23
raut-abhishek <iamabhishekraut01@gmail.com>
2026-04-27 12:03:26 +0530

    Prebuilt Predicate functional interface in java

---
+++ java/4-23-2026/PredicateInterface.java
@@ -0,0 +1,25 @@
+import java.util.function.Predicate;
+
+import javax.swing.TransferHandler.TransferSupport;
+
+public class PredicateInterface {
+    public static void main(String [] args){
+        Predicate <Integer> n = (Integer a)->{
+            return a==100;
+        };
+        System.out.println(n.test(100));
+        System.out.println(n.test(1000));
+
+        Predicate<String> s = (String s1)->{
+            String result = "";
+            for(int i = 0; i<s1.length(); i++){
+                result += s1.charAt(i);
+            }
+            return s1==result;
+        };
+
+        System.out.println(s.test("INDIA"));
+        System.out.println(s.test("MADAM"));
+    }
+
+}
