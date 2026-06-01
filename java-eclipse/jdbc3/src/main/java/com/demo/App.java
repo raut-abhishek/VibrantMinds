ceeb7edd96e3e5163a1adf2690f5474612551a97
raut-abhishek <iamabhishekraut01@gmail.com>
2026-06-01 11:30:03 +0530

    insert student and delete student methods invoke

--- java-eclipse/jdbc3/src/main/java/com/demo/App.java
+++ java-eclipse/jdbc3/src/main/java/com/demo/App.java
@@ -1,5 +1,8 @@
 package com.demo;
 
+import com.dao.StudentDao;
+import com.model.Student;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        
+    	StudentDao sd = new StudentDao();
+//    	Student s = new Student();
+//    	s.setName("Shreyas");
+//    	s.setCity("Nagpur");
+//    	s.setPercentage(50.23);
+//    	System.out.println(sd.insertStudent(s));
+    	
+    	System.out.println(sd.deleteStudentById(5005));
+    	
+    	
     }
+    
+    
 }
