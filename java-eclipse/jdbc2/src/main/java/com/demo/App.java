abd2abdcfcdd1e574983f77f5f835a9579d7b3b7
raut-abhishek <iamabhishekraut01@gmail.com>
2026-06-01 10:17:21 +0530

    removed random records inserting code and delete student by id method invoked

--- java-eclipse/jdbc2/src/main/java/com/demo/App.java
+++ java-eclipse/jdbc2/src/main/java/com/demo/App.java
@@ -1,7 +1,6 @@
 package com.demo;
 
 import java.util.List;
-import java.util.Random;
 
 import com.dao.StudentDao;
 import com.model.Student;
@@ -42,41 +41,11 @@ public class App
 //   	    for(Student s: list){
 //            System.out.println(s);
 //        }
-
-    	String[] cities = {
-    		    "Mumbai", "Delhi", "Bengaluru", "Hyderabad", "Ahmedabad",
-    		    "Chennai", "Kolkata", "Pune", "Jaipur", "Surat",
-    		    "Lucknow", "Kanpur", "Nagpur", "Indore", "Thane",
-    		    "Bhopal", "Visakhapatnam", "Patna", "Vadodara", "Ghaziabad",
-    		    "Ludhiana", "Agra", "Nashik", "Faridabad", "Meerut",
-    		    "Rajkot", "Kalyan", "Vasai", "Varanasi", "Srinagar",
-    		    "Aurangabad", "Dhanbad", "Amritsar", "Navi Mumbai", "Allahabad",
-    		    "Ranchi", "Howrah", "Coimbatore", "Jabalpur", "Gwalior",
-    		    "Vijayawada", "Jodhpur", "Madurai", "Raipur", "Kota",
-    		    "Guwahati", "Chandigarh", "Solapur", "Hubli", "Mysuru",
-    		    "Tiruchirappalli", "Bareilly", "Aligarh", "Tiruppur", "Moradabad",
-    		    "Jalandhar", "Bhubaneswar", "Salem", "Warangal", "Guntur",
-    		    "Bhiwandi", "Saharanpur", "Gorakhpur", "Bikaner", "Amravati",
-    		    "Noida", "Jamshedpur", "Bhilai", "Cuttack", "Firozabad",
-    		    "Kochi", "Nellore", "Bhavnagar", "Dehradun", "Durgapur",
-    		    "Asansol", "Rourkela", "Nanded", "Kolhapur", "Ajmer",
-    		    "Akola", "Gulbarga", "Jamnagar", "Ujjain", "Loni",
-    		    "Siliguri", "Jhansi", "Ulhasnagar", "Jammu", "Sangli",
-    		    "Mangalore", "Erode", "Belagavi", "Ambattur", "Tirunelveli",
-    		    "Malegaon", "Gaya", "Jalgaon", "Udaipur", "Karnal"
-    		};
     	
-    	Random r = new Random();
-    	for(int i = 0; i<=5000; i++) {
-    		Student s = new Student();
-    		StringBuilder name = new StringBuilder();
-    		for(int j = 0; j<=15; j++) {
-    			name.append((char) (r.nextInt(26)+65));
-    		}
-    		s.setName(name.toString());
-    		s.setCity(cities[r.nextInt(cities.length)]);
-    		s.setPercentage(r.nextFloat()*100);
-    		System.out.println(sd.insertStudent(s));
-    	}
+//    	DELETE STUDENT BY ID
+    	
+    	System.out.println(sd.deleteStudentById(5006));
+
+
     }
 }
