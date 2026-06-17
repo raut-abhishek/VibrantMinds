c53a441abbb6cc18625d445eb083d6004bf5ca44
raut-abhishek <iamabhishekraut01@gmail.com>
2026-06-17 13:38:19 +0530

    Implemented input methods and login interface

--- java-eclipse/Grocery_Management/src/main/java/com/demo/App.java
+++ java-eclipse/Grocery_Management/src/main/java/com/demo/App.java
@@ -1,13 +1,41 @@
 package com.demo;
 
-/**
- * Hello world!
- *
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import com.dao.UserDao;
+import com.exception.UserNotFoundException;
+import com.model.User;
+
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+    	UserDao ud = new UserDao();
+    	User u = null;
+    	
+//    	Input methods
+    	InputStreamReader isr = new InputStreamReader(System.in);
+    	BufferedReader br = new BufferedReader(isr);
+    	
+    	
+//    	Login system
+    	System.out.println("Hey Welcome to the store!!");
+    	System.out.println("Please login to continue shopping.");
+    	String inputUserName = null;
+    	String inputPassword = null;
+    	try {
+    		System.out.print("Username : ");
+			inputUserName = br.readLine();
+			System.out.print("Password : ");
+			inputPassword = br.readLine();
+			u = ud.login(inputUserName, inputPassword);
+			System.out.println(u);
+			
+		} catch ( UserNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+    	
     }
 }
