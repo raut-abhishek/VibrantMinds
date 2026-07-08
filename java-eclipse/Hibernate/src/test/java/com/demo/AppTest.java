acaf27641bb54040ab2638908a3cf6c3b0d4cff3
raut-abhishek <iamabhishekraut01@gmail.com>
2026-07-08 19:58:16 +0530

    initial maven progect for hibernate implimentation

---
+++ java-eclipse/Hibernate/src/test/java/com/demo/AppTest.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class AppTest 
+{
+   
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
